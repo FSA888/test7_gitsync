5496824de5547753895ffba4aed304f89ee4c688
sergey.fedorchuk <sergey.fedorchuk.1978@yandex.ru>
2024-08-12 01:12:57 +0700

    Revert "init"
    This reverts commit 28bef60cc39e766408dad333523eb2e725dd36e9.

--- gitsync_run.cmd
+++ gitsync_run.cmd
@@ -5,3 +5,7 @@ gitsync init -u gitbot -p 123 D:\1C\1Cbases\Финансы_Store E:\GitHub\test7
 ## Синхронизация с хранилищем 1С
 
 gitsync sync -u gitbot -p 123 D:\1C\1Cbases\Финансы_Store E:\GitHub\test7_gitsync
+
+## Сформировать релиз
+
+vrunner compile --src src/cf --out build/1cv8.cf
